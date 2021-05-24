7487e0c92c8c380b84950e5c575356dac7c11966
Олег Андрианов <ooorivas@hotmail.com>
2021-05-24 11:15:09 +0500

    begin

--- src/process_sensor.c
+++ src/process_sensor.c
@@ -12,78 +12,136 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(sensor);
 
-struct sensor_conf {
-	const char *name;
-	const uint16_t od;
+
+struct sensor_data{
 	const struct device *dev;
 	void  *od_data;
 };
 
-static struct sensor_conf bh1750[] = {
+struct sensor_config;
+typedef int (*sample_fetch_fn) (const struct sensor_config* sens);
+
+struct sensor_config {
+	const char *name;
+	const uint16_t od;
+    const sample_fetch_fn sample_fetch;
+	struct sensor_data * const data;
+};
+
+#ifdef OD_2006_BH1750
+   static struct sensor_data sd_2006_BH1750;
+#endif
+#ifdef OD_2007_BH1750
+   static struct sensor_data sd_2007_BH1750;
+#endif
+#ifdef OD_2008_BH1750
+   static struct sensor_data sd_2008_BH1750;
+#endif
+#ifdef OD_2009_BH1750
+   static struct sensor_data sd_2009_BH1750;
+#endif
+#ifdef OD_200C_BME280
+   static struct sensor_data sd_200C_BME280;
+#endif
+#ifdef OD_200D_BME280
+   static struct sensor_data sd_200D_BME280;
+#endif
+#ifdef OD_200E_BME280
+   static struct sensor_data sd_200E_BME280;
+#endif
+#ifdef OD_200F_BME280
+   static struct sensor_data sd_200F_BME280;
+#endif
+#ifdef OD_200A_AM2320
+   static struct sensor_data sd_200A_AM2320;
+#endif
+#ifdef OD_200B_AM2320
+   static struct sensor_data sd_200B_AM2320;
+#endif
+
+static int sample_fetch_BH1750(const struct sensor_config * sens);
+static int sample_fetch_BME280(const struct sensor_config * sens);
+static int sample_fetch_AM2320(const struct sensor_config * sens);
+
+static const struct sensor_config allSensor[] = {
 #ifdef OD_2006_BH1750
     {
         .name="BH1750_1",
         .od=OD_2006_BH1750,
+        .sample_fetch=sample_fetch_BH1750,
+        .data = &sd_2006_BH1750
      },
 #endif
 #ifdef OD_2007_BH1750
     {
         .name="BH1750_2",
         .od=OD_2007_BH1750,
+        .sample_fetch=sample_fetch1_BH1750,
+        .data = &sd_2007_BH1750
      },
 #endif
 #ifdef OD_2008_BH1750
     {
         .name="BH1750_3",
         .od=OD_2008_BH1750,
+        .sample_fetch=sample_fetch1_BH1750,
+        .data = &sd_2008_BH1750
      },
 #endif
 #ifdef OD_2009_BH1750
     {
         .name="BH1750_4",
         .od=OD_2009_BH1750,
+        .sample_fetch=sample_fetch_BH1750,
+        .data = &sd_2009_BH1750
      },
-#endif//*/
-};
-
-static struct sensor_conf bme280[] = {
+#endif
 #ifdef OD_200C_BME280
     {
         .name="BME280_1",
         .od=OD_200C_BME280,
+        .sample_fetch=sample_fetch_BME280,
+        .data = &sd_200C_BME280
      },
 #endif
 #ifdef OD_200D_BME280
     {
         .name="BME280_2",
         .od=OD_200D_BME280,
+        .sample_fetch=sample_fetch_BME280,
+        .data = &sd_200D_BME280
      },
 #endif
 #ifdef OD_200E_BME280
     {
         .name="BME280_3",
         .od=OD_200E_BME280,
+        .sample_fetch=sample_fetch_BME280,
+        .data = &sd_200E_BME280
      },
 #endif
 #ifdef OD_200F_BME280
     {
         .name="BME280_4",
         .od=OD_200F_BME280,
+        .sample_fetch=sample_fetch_BME280,
+        .data = &sd_200F_BME280
      },
 #endif
-};
-
-static struct sensor_conf am2320[] = {
 #ifdef OD_200A_AM2320
     {
         .name="AM2320_1",
         .od=OD_200A_AM2320,
+        .sample_fetch=sample_fetch_AM2320,
+        .data = &sd_200A_AM2320
      },
 #endif
 #ifdef OD_200B_AM2320
     {
         .name="AM2320_2",
         .od=OD_200B_AM2320,
+        .sample_fetch=sample_fetch_AM2320,
+        .data = &sd_200B_AM2320
      },
 #endif
 };
@@ -102,27 +160,29 @@ static inline const struct device *to_bus(const struct device *dev)
 	return to_config(dev)->bus;
 }
 
-static int sample_fetch(struct sensor_conf * sens)
+static int sample_fetch(const struct sensor_config * sens)
 {
-        if (sens->dev)
+    const struct device * d = sens->data->dev;
+        if (d)
         {
             int err;
-		    err = sensor_sample_fetch(sens->dev);
+		    err = sensor_sample_fetch(d);
             if (err < 0)
             {
                 LOG_ERR("Error: Device %s", sens->name);
-                i2c_recover_bus(to_bus(sens->dev));
+                i2c_recover_bus(to_bus(d));
             }
             return err;
         }
         else return -1;
 }
 
-static int sample_fetch_BH1750(struct sensor_conf * sens)
+static int sample_fetch_BH1750(const struct sensor_config * sens)
 {
 
     int ret = sample_fetch(sens);
-    OD_BH1750_t *od = sens->od_data;
+    OD_BH1750_t *od = sens->data->od_data;
+    const struct device * d = sens->data->dev;
 
     if (ret < 0){
         CO_LOCK_OD();
@@ -131,18 +191,20 @@ static int sample_fetch_BH1750(struct sensor_conf * sens)
     }
     else {
         struct sensor_value lux;
-	    sensor_channel_get(sens->dev, SENSOR_CHAN_LIGHT, &lux);
+	    sensor_channel_get(d, SENSOR_CHAN_LIGHT, &lux);
         CO_LOCK_OD();
         od->lux = lux.val1;
         CO_UNLOCK_OD();
     }
     return ret;
 }
-static int sample_fetch_BME280(struct sensor_conf * sens)
+
+static int sample_fetch_BME280(const struct sensor_config * sens)
 {
 
     int ret = sample_fetch(sens);
-    OD_BME280_t *od = sens->od_data;
+    OD_BME280_t *od = sens->data->od_data;
+    const struct device * d = sens->data->dev;
 
     if (ret < 0){
         CO_LOCK_OD();
@@ -153,15 +215,15 @@ static int sample_fetch_BME280(struct sensor_conf * sens)
     }
     else {
         struct sensor_value sv;
-	    sensor_channel_get(sens->dev, SENSOR_CHAN_AMBIENT_TEMP, &sv);
+	    sensor_channel_get(d, SENSOR_CHAN_AMBIENT_TEMP, &sv);
         CO_LOCK_OD();
         od->temp = sv.val1;
         CO_UNLOCK_OD();
-	    sensor_channel_get(sens->dev, SENSOR_CHAN_HUMIDITY, &sv);
+	    sensor_channel_get(d, SENSOR_CHAN_HUMIDITY, &sv);
         CO_LOCK_OD();
         od->humid = sv.val1;
         CO_UNLOCK_OD();
-	    sensor_channel_get(sens->dev, SENSOR_CHAN_PRESS, &sv);
+	    sensor_channel_get(d, SENSOR_CHAN_PRESS, &sv);
         int32_t press = (sv.val1*1000 + sv.val2/1000)/133;
         CO_LOCK_OD();
         od->press = press;
@@ -169,11 +231,13 @@ static int sample_fetch_BME280(struct sensor_conf * sens)
     }
     return ret;
 }
-static int sample_fetch_AM2320(struct sensor_conf * sens)
+
+static int sample_fetch_AM2320(const struct sensor_config * sens)
 {
 
     int ret = sample_fetch(sens);
-    OD_BME280_t *od = sens->od_data;
+    OD_AM2320_t *od = sens->data->od_data;
+    const struct device * d = sens->data->dev;
 
     if (ret < 0){
         CO_LOCK_OD();
@@ -183,11 +247,11 @@ static int sample_fetch_AM2320(struct sensor_conf * sens)
     }
     else {
         struct sensor_value sv;
-	    sensor_channel_get(sens->dev, SENSOR_CHAN_AMBIENT_TEMP, &sv);
+	    sensor_channel_get(d, SENSOR_CHAN_AMBIENT_TEMP, &sv);
         CO_LOCK_OD();
         od->temp = sv.val1;
         CO_UNLOCK_OD();
-	    sensor_channel_get(sens->dev, SENSOR_CHAN_HUMIDITY, &sv);
+	    sensor_channel_get(d, SENSOR_CHAN_HUMIDITY, &sv);
         CO_LOCK_OD();
         od->humid = sv.val1;
         CO_UNLOCK_OD();
@@ -195,115 +259,85 @@ static int sample_fetch_AM2320(struct sensor_conf * sens)
     return ret;
 }
 
-static const struct device* check_dev(struct sensor_conf      *sens,
-                                        void                  *od_data)
+static const struct device* check_dev(const struct sensor_config* sens,void *od_data)
 {
-    sens->dev = device_get_binding(sens->name);
-    sens->od_data = od_data;
+    const struct device * d = device_get_binding(sens->name);
+    sens->data->od_data = od_data;
 
-	if (sens->dev == NULL) {
+	if (d == NULL) {
 		// No such node, or the node does not have status "okay".
 		LOG_ERR("%s Error: no device found.", sens->name);
-        return NULL;
+        // return NULL;
 	}
     else {
-	    const struct device* bus = to_bus(sens->dev);
+	    const struct device* bus = to_bus(d);
 
-	    if (!device_is_ready(sens->dev)) {
+	    if (!device_is_ready(d)) {
 		    LOG_ERR("Error: Device \"%s %s\" is not ready; "
                     "check the driver initialization logs for errors.",
-		            sens->dev->name, bus->name);
-		    return NULL;
+		            d->name, bus->name);
+		    // return NULL;
 	    }
-	    LOG_DBG("Found device \"%s  %s\", getting sensor data\n", sens->dev->name, bus->name);
+	    LOG_DBG("Found device \"%s  %s\", getting sensor data\n", d->name, bus->name);
     }
-    return sens->dev;
+    sens->data->dev = d;
+    return d;
 }
+
 
 void sensors_init(void)
 {     
-     struct sensor_conf *sens = bh1750;
-     OD_BH1750_t* od_BH1750 = (OD_BH1750_t*) &OD_BH1750;
+    const struct sensor_config *sens = allSensor;
 
-     for (size_t i = 0; i < ARRAY_SIZE(bh1750); i++)
-     {
-       check_dev(sens, od_BH1750);//, BH1750_odf_2006_9);
-       sample_fetch_BH1750(sens);
-       od_BH1750++;
-       sens++;
-     }
-
-     sens = bme280;     
-     OD_BME280_t* od_BME280 = (OD_BME280_t*) &OD_BME280;
-
-     for (size_t i = 0; i < ARRAY_SIZE(bme280); i++)
-     {
-       check_dev(sens, od_BME280);//, BME280_odf_200C_F);
-       sample_fetch_BME280(sens);
-       od_BME280++;
-       sens++;
-     }
-
-     sens = am2320;     
-     OD_AM2320_t* od_AM2320 = (OD_AM2320_t*) &OD_AM2320;
-
-     for (size_t i = 0; i < ARRAY_SIZE(am2320); i++)
-     {
-       check_dev(sens, od_AM2320);//, AM2320_odf_200A_B);
-       sample_fetch_AM2320(sens);
-       od_AM2320++;
-       sens++;
-     }
+    #ifdef OD_BH1750   
+        OD_BH1750_t* od_BH1750 = (OD_BH1750_t*) &OD_BH1750;
+        while (sens->sample_fetch == sample_fetch_BH1750)
+        {
+            check_dev(sens, od_BH1750);
+            sens->sample_fetch(sens);
+            od_BH1750++;
+            sens++;
+        };
+    #endif
+    #ifdef OD_BME280  
+        OD_BME280_t* od_BME280 = (OD_BME280_t*) &OD_BME280;
+        while (sens->sample_fetch == sample_fetch_BME280)
+        {
+            check_dev(sens, od_BME280);
+            sens->sample_fetch(sens);
+            od_BME280++;
+            sens++;
+        };
+    #endif
+    #ifdef OD_AM2320   
+        OD_AM2320_t* od_AM2320 = (OD_AM2320_t*) &OD_AM2320;
+        while (sens->sample_fetch == sample_fetch_AM2320)
+        {
+            check_dev(sens, od_AM2320);
+            sens->sample_fetch(sens);
+            od_AM2320++;
+            sens++;
+        };
+    #endif
 }
 
 void sensors_process(uint16_t timeDifference_ms)
 {
     static uint16_t timer;
-    static struct sensor_conf *sens;
-    static int8_t index_type;
+    static const struct sensor_config *sens;
     static int8_t index;
 
     timer += timeDifference_ms;
     if(timer >= CONFIG_SENSORS_UPDATE_TIME){
         timer -= CONFIG_SENSORS_UPDATE_TIME;
-        sens = bh1750;
-        index_type = 3;
-        index = ARRAY_SIZE(bh1750);
+        sens = allSensor;
+        index = ARRAY_SIZE(allSensor);
     }
-    if (index_type)
-    {
-        if (index > 0)
-        {
-            switch (index_type)
-            {
-                case 3:
-                    sample_fetch_BH1750(sens++);
-                break;
-                case 2:
-                    sample_fetch_BME280(sens++);
-                break;        
-                case 1:
-                    sample_fetch_AM2320(sens++);
-                break;
-            }
-        }
-        if(--index <= 0)
-        {
-            index_type--;
 
-            if (index_type)
-            {
-                if (index_type == 2)
-                {
-                    index = ARRAY_SIZE(bme280);                
-                    sens = bme280;     
-                }
-                else 
-                {
-                    index = ARRAY_SIZE(am2320);            
-                    sens = am2320;     
-                }
-            }
-        }                
+    if (index)
+    {
+        sens->sample_fetch(sens);
+        index--;
+        sens++;
     }
 }
